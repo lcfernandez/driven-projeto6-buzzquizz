c7a80dd48a8f2e6d6112ccdd9c2d341bc04b60c1
andherpaulo <andher.dev@gmail.com>
2022-08-31 11:32:17 -0300

    Adição do arquivo .wakatime e criação da homepage

--- script.js
+++ script.js
@@ -10,17 +10,49 @@
 
 - events */
 
+/* ---------- global variables ---------- */
 
-/* global variables */
+const urlApi = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
 
+/* ---------- assignments ---------- */
 
-/* assignments */
+/* ---------- calls ---------- */
 
+listQuizzes();
 
-/* calls */
+/* ---------- functions ---------- */
 
+// Homepage
 
-/* functions */
+function listQuizzes() {
+    const quizzListElement = document.querySelector(".c-quizzes-list");
+    quizzListElement.innerHTML = "";
 
+    axios
+        .get(urlApi)
+        .then((res) => {
+            console.log(res.data);
+            res.data.forEach((quizz) => {
+                console.log(quizz);
+                quizzListElement.innerHTML += `
+                    <li class="c-quizzes-list__item" onclick="openQuizz(this)">
+                        <img
+                            src="${quizz.image}"
+                        />
+                        <div class="c-quizzes-list__gradient"></div>
+                        <p>${quizz.title}</p>
+                    </li>
+                `;
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
 
-/* events */
+function openQuizz(quizz) {
+    const homepage = document.querySelector(".c-homepage");
+    homepage.classList.add("is-inactive");
+}
+
+/* ---------- events ---------- */
