a8692ae83db65d98c9a3dfedd9c47f6334af98ee
andherpaulo <andher.dev@gmail.com>
2022-08-31 16:39:57 -0300

    Adição do layout da página de quizz

--- script.js
+++ script.js
@@ -13,6 +13,8 @@
 /* ---------- global variables ---------- */
 
 const urlApi = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes";
+const quizzpage = document.querySelector(".c-quizzpage");
+let quizzAnswersLi = "";
 
 /* ---------- assignments ---------- */
 
@@ -31,16 +33,18 @@ function listQuizzes() {
     axios
         .get(urlApi)
         .then((res) => {
-            console.log(res.data);
             res.data.forEach((quizz) => {
-                console.log(quizz);
                 quizzListElement.innerHTML += `
-                    <li class="c-quizzes-list__item" onclick="openQuizz(this)">
+                    <li
+                        class="c-quizzes-list__item"
+                        onclick="openQuizz(this)"
+                        data-id="${quizz.id}"
+                    >
                         <img
                             src="${quizz.image}"
                         />
                         <div class="c-quizzes-list__gradient"></div>
-                        <p>${quizz.title}</p>
+                        <h2>${quizz.title}</h2>
                     </li>
                 `;
             });
@@ -50,9 +54,66 @@ function listQuizzes() {
         });
 }
 
+function getAnswers(answer) {
+    answer.forEach(()=>{
+        console.log(answer)
+        /* quizzAnswersLi += `
+            <li class="c-question__answer">
+                <img src="${answer.image}" alt="Representação da opção de resposta">
+                <p>${answer.text}</p>
+            </li>
+        `; */
+    });
+}
+
 function openQuizz(quizz) {
+    const quizzId = quizz.dataset.id;
+
     const homepage = document.querySelector(".c-homepage");
     homepage.classList.add("is-inactive");
+
+    quizzpage.classList.remove("is-inactive");
+
+    quizzpage.innerHTML = "";
+    quizzAnswersLi = "";
+
+    axios
+        .get(`${urlApi}/${quizzId}`)
+        .then((res) => {
+            console.log(res.data);
+
+            quizzpage.innerHTML = `
+            <header class="c-quizzpage__header">
+                <img src="${res.data.image}" alt="Capa do quizz" />
+                <div class="c-quizzpage__header__foreground"></div>
+                <h2>${res.data.title}</h2>
+            </header>
+        `;
+
+            res.data.questions.forEach((answer) => {
+                getAnswers(answer.answers);
+            });
+
+            res.data.questions.forEach((question) => {
+                quizzpage.innerHTML += `
+                <div class="c-question">
+                    <div class="c-question__title">
+                        <h3>${question.title}</h3>
+                    </div>
+                    <ul class="c-question__options">
+                `;
+
+                question.answers.forEach(renderAnswers);
+
+                quizzpage.innerHTML += `
+                    </ul>
+                </div>
+            `;
+            });
+        })
+        .catch((err) => {
+            console.error(err);
+        });
 }
 
 /* ---------- events ---------- */
